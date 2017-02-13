d64ce59e29e2bc7572a14fee6b22a0fa2f5a3926
1724404960 <1724404960@qq.com>
2017-02-13 11:36:39 +0800

    Require

---
+++ RequireJs/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by HelloZcq on 2017/2/13.
+ */
+//require(["a"],function(arrSort){
+//    var arr=[1,13,9,11];
+//    console.log(arrSort(arr));
+//})
+//sea.js
+
+
+define(function(require){
+    var arr=[1,13,9,11];
+    var arrSort=require("a");//注意此处不加中括号
+    console.log(arrSort(arr))
+});
