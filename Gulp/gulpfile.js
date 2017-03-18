c6f13ba2d7dc61d74c692cf04c32625ebb4881ee
1724404960 <1724404960@qq.com>
2017-03-19 06:31:06 +0800

    gulp的使用

---
+++ Gulp/gulpfile.js
@@ -0,0 +1,46 @@
+/**
+ * Created by HelloZcq on 2017/3/18.
+ */
+var gulp=require('gulp');
+var sass=require('gulp-sass');
+var connect=require('gulp-connect');
+gulp.task('task',function(){
+    console.log('aaa')
+});
+gulp.task('task1',function(){
+    console.log('bbb')
+});
+gulp.task('task2',['task1','task']);
+gulp.task('copy',function () {
+    gulp.src('index.html').pipe(gulp.dest('dist'))
+    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
+});
+gulp.task('copy-img',function () {
+    gulp.src('src/images/*.jpg').pipe(gulp.dest('dist'));
+    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
+});
+gulp.task('copy-img',function () {
+    gulp.src(['src/images/**/*.{jpg,png}','!src/images/4.png']).pipe(gulp.dest('dist/imgss'));
+    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
+});
+gulp.task('copy-img',function () {
+    gulp.src(['src/images.{jpg,png}','!src/images/4.png']).pipe(gulp.dest('dist/imgss'));
+    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所-以必须得写pipe
+});
+gulp.task('sass',function () {
+    gulp.src('src/sass/*.sass').pipe(sass()).pipe(gulp.dest('dest/css'));
+    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
+});
+gulp.task('watch-html',function () {
+    gulp.watch('index.html',['copy-html']);4
+});
+gulp.task('copy-html',function () {
+    gulp.src('index.html').pipe(gulp.dest('new1')).pipe(connect.reload());
+});
+gulp.task('server',function () {
+    connect.server({
+        root:'dist',
+        livereload:true
+    })
+});
+gulp.task('default',['server','watch-html']);
