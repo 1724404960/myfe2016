f6fe94dfa90c2c793b1450ff758c065a7e145f19
1724404960 <1724404960@qq.com>
2017-03-22 07:26:04 +0800

    gulp

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -11,28 +11,31 @@ gulp.task('task1',function(){
     console.log('bbb')
 });
 gulp.task('task2',['task1','task']);
-gulp.task('copy',function () {
+gulp.task('copy-html',function () {
     gulp.src('index.html').pipe(gulp.dest('dist'))
+    console.log('bbb')
     //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
 });
-gulp.task('copy-img',function () {
-    gulp.src('src/images/*.jpg').pipe(gulp.dest('dist'));
-    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
+gulp.task('copy-img1',function () {
+    gulp.src('src/icons/images/*.jpg').pipe(gulp.dest('dist'));
 });
-gulp.task('copy-img',function () {
-    gulp.src(['src/images/**/*.{jpg,png}','!src/images/4.png']).pipe(gulp.dest('dist/imgss'));
-    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
-});
-gulp.task('copy-img',function () {
-    gulp.src(['src/images.{jpg,png}','!src/images/4.png']).pipe(gulp.dest('dist/imgss'));
-    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所-以必须得写pipe
+// gulp.task('copy-img',function () {
+//     gulp.src(['src/images/**/*.{jpg,png}','!src/images/4.png']).pipe(gulp.dest('dist/imgss'));
+//     //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
+// });
+// gulp.task('copy-img',function () {
+//     gulp.src(['src/images.{jpg,png}','!src/images/4.png']).pipe(gulp.dest('dist/imgss'));
+//     //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所-以必须得写pipe
+// });
+// gulp.task('sass',function () {
+//     gulp.src('src/sass/*.sass').pipe(sass()).pipe(gulp.dest('dest/css'));
+//     //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
+// });
+gulp.task('watch-html',function () {
+    gulp.watch('index.html',['copy-html']);
 });
 gulp.task('sass',function () {
-    gulp.src('src/sass/*.sass').pipe(sass()).pipe(gulp.dest('dest/css'));
-    //找到该文件把它放在dist文件夹下，这个操作过程都是由管道来完成的，所以必须得写pipe
-});
-gulp.task('watch-html',function () {
-    gulp.watch('index.html',['copy-html']);4
+    gulp.src('src/sass/*.sass').pipe(sass()).pipe(gulp.dest('dist/css'));
 });
 gulp.task('copy-html',function () {
     gulp.src('index.html').pipe(gulp.dest('new1')).pipe(connect.reload());
